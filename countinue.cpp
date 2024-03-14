6832f0b73aac71a09c3d9659d4133d5270e36895
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-14 20:23:21 +0530

    continue

---
+++ countinue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;//use continue
+int main(){
+int n;
+cout<<"Enter the value  of n"<<endl;
+cin>>n;
+int i=1;
+for(int i=0; i<5; i++){
+  cout<<"Hi"<<endl;
+  cout<<"Hey";
+  continue;
+  cout<<"reply to ker de"<<endl;
+}
+};
