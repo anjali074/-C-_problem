be4dd1042047d3c7c8d18e805590d2c1d00f606e
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-05-10 11:24:12 +0530

    all datatype print

---
+++ Array3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+//created function
+void printArray(int arr[], int  size){
+cout <<"printing the array"<<endl;
+for(int i=0; i<5; i++){
+//cout<<i[3]<<"  ";
+}
+cout<<"printed is done"<<endl;
+}
+int main(){
+  /*int third[15]={2,7};
+  int n=15;
+printArray(third,15);
+  int thirdsize=sizeof(third)/sizeof(int);
+  cout<<"size of third is"<<third<<endl;*/
+  char ch[5]={'a','b','c','d'};
+  cout<<ch[5]<<endl;
+  for(int i=0; i<5; i++){
+cout<<ch[i]<<"  ";
+}
+double firstDouble[5];
+float firstFloat[6];
+bool firstBool[9];
+}
